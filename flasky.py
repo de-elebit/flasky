9fdc8ec574ea76d353feeb444d27be34ec994f10
De.Elebit <De.Elebit@gmail.com>
2019-10-31 23:11:54 +0900

    Add : User Confirmation

--- flasky.py
+++ flasky.py
@@ -7,7 +7,6 @@ app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
@@ -19,7 +18,6 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
 
 
 if __name__ == '__main__':
